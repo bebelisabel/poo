a0f2c954c87bc80c474993394662fa7bf4d39734
bebelisabel <garcia01bebel@gmail.com>
2026-02-04 11:43:06 -0300

    Add files via upload

---
+++ biblioeteca/biblioeteca/src/main/java/br/com/senai/backend/biblioeteca/BiblioetecaApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.backend.biblioeteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioetecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioetecaApplication.class, args);
+	}
+
+}
---
+++ biblioeteca/biblioeteca/src/test/java/br/com/senai/backend/biblioeteca/BiblioetecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.backend.biblioeteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioetecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
